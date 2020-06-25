3cc75f0404289381391dce2c1bc531655fbd4e01
msi <smsaifmorshed@gmail.com>
2020-06-25 11:52:24 +0600

    Updates

---
+++ precache-manifest.b11e83fe6539c086335686e3c82c4fc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad40d1a3bcef0a9ebfa47aca899345f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d0c1e233e25246c84f93",
+    "url": "/monsters-rolodex/static/css/main.53820c20.chunk.css"
+  },
+  {
+    "revision": "48792edbdf70cf9871f4",
+    "url": "/monsters-rolodex/static/js/2.2c69bc30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.2c69bc30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c1e233e25246c84f93",
+    "url": "/monsters-rolodex/static/js/main.c8067326.chunk.js"
+  },
+  {
+    "revision": "87e14fa37cfad054dbe3",
+    "url": "/monsters-rolodex/static/js/runtime-main.191be594.js"
+  }
+]);
